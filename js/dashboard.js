b57b56a71825a14641387e46eff6c7ed9b3610cc
Guillaume Aubert <aubertg@cpan.org>
2016-05-02 20:43:13 -0700

    Add sections for better semantic grouping.

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@ $(document).ready(
 	function()
 	{
 		// Create table of CPAN distributions.
-		$('#cpan_distributions').createDistributionsList(
+		$('#cpan_distributions table').createDistributionsList(
 			{
 				pause_id: "AUBERTG",
 				github_id: "guillaumeaubert",
