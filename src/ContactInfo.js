780e7dceb5423a92742405191b7509d781bea229
Guillaume Aubert <aubertg@cpan.org>
2019-03-10 23:17:09 -0700

    Make components functional.

--- src/ContactInfo.js
+++ src/ContactInfo.js
@@ -1,40 +1,38 @@
 // @flow strict
 
+import type { Node } from 'react';
+
 import React from 'react';
 import githubIcon from './images/font-awesome_4-6-3_github_32_0_ffffff_none.png';
 import linkedinIcon from './images/brandico_2014-04-07_linkedin-rect_32_0_ffffff_none.png';
 import envelopeIcon from './images/font-awesome_4-6-3_envelope-square_32_0_ffffff_none.png';
 import './ContactInfo.css';
 
-type Props = {||};
-
-class ContactInfo extends React.Component<Props> {
-  render() {
-    return (
-      <div id="contact-info">
-        <ul>
-          <li>
-            Find me on:
-          </li>
-          <li>
-            <a href="https://github.com/guillaumeaubert/">
-              <img src={githubIcon} alt="Github"/>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/aubertguillaume">
-              <img src={linkedinIcon} alt="LinkedIn"/>
-            </a>
-          </li>
-          <li>
-            <a href="mailto:me@aubertg.com">
-              <img src={envelopeIcon} alt="Email"/>
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const ContactInfo = (): Node => {
+  return (
+    <div id="contact-info">
+      <ul>
+        <li>
+          Find me on:
+        </li>
+        <li>
+          <a href="https://github.com/guillaumeaubert/">
+            <img src={githubIcon} alt="Github"/>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/aubertguillaume">
+            <img src={linkedinIcon} alt="LinkedIn"/>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:me@aubertg.com">
+            <img src={envelopeIcon} alt="Email"/>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default ContactInfo;
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,7 @@
 // @flow strict
 
+import type { Node } from 'react';
+
 import React from 'react';
 import {
   Route,
@@ -12,56 +14,52 @@ import ContactInfo from './ContactInfo';
 import VerticalTextGenerator from './VerticalTextGenerator';
 import './Main.css';
 
-type Props = {||};
+const Main = (): Node => {
+  return (
+    <HashRouter>
+      <div>
+        <div id="header_wrap" className="outer">
+          <header className="inner">
+            <ContactInfo />
 
-class Main extends React.Component<Props> {
-  render() {
-    return (
-      <HashRouter>
-        <div>
-          <div id="header_wrap" className="outer">
-            <header className="inner">
-              <ContactInfo />
-
-              <h1>Guillaume Aubert</h1>
-              <section id="menu">
-                <NavLink exact to="/">
-                  <span className="bullet">&#9700;&nbsp;</span>
-                  My Projects
-                </NavLink>
-                <NavLink to="/git-contributions-statistics">
-                  <span className="bullet">&#9700;&nbsp;</span>
-                  Git Contribution Statistics
-                </NavLink>
-                <NavLink to="/vertical-text-generator">
-                  <span className="bullet">&#9700;&nbsp;</span>
-                  Vertical Text Generator
-                </NavLink>
-              </section>
-            </header>
-          </div>
-          <div id="main_content_wrap" className="outer">
-            <section id="main_content" className="inner">
-              <div className="content">
-                <Route
-                  exact path="/"
-                  render={() => <MyProjects githubUser='guillaumeaubert' dockerhubUser='aubertg'/>}
-                />
-                <Route
-                  path="/git-contributions-statistics"
-                  component={GitStats}
-                />
-                <Route
-                  path="/vertical-text-generator"
-                  render={() => <VerticalTextGenerator text="DEVASTATING" transformation="square"/>}
-                />
-              </div>
+            <h1>Guillaume Aubert</h1>
+            <section id="menu">
+              <NavLink exact to="/">
+                <span className="bullet">&#9700;&nbsp;</span>
+                My Projects
+              </NavLink>
+              <NavLink to="/git-contributions-statistics">
+                <span className="bullet">&#9700;&nbsp;</span>
+                Git Contribution Statistics
+              </NavLink>
+              <NavLink to="/vertical-text-generator">
+                <span className="bullet">&#9700;&nbsp;</span>
+                Vertical Text Generator
+              </NavLink>
             </section>
-          </div>
+          </header>
         </div>
-      </HashRouter>
-    );
-  }
-}
+        <div id="main_content_wrap" className="outer">
+          <section id="main_content" className="inner">
+            <div className="content">
+              <Route
+                exact path="/"
+                render={() => <MyProjects githubUser='guillaumeaubert' dockerhubUser='aubertg'/>}
+              />
+              <Route
+                path="/git-contributions-statistics"
+                component={GitStats}
+              />
+              <Route
+                path="/vertical-text-generator"
+                render={() => <VerticalTextGenerator text="DEVASTATING" transformation="square"/>}
+              />
+            </div>
+          </section>
+        </div>
+      </div>
+    </HashRouter>
+  );
+};
 
 export default Main;
